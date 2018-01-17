a746ed1712116c4766f63b20df7edc1f989d3da3
seanmorgan <seanmorgan@gmail.com>
2018-01-17 14:32:17 -0500

    Added logout button

--- src/main/java/com/example/simpleoauth/SimpleOauthApplication.java
+++ src/main/java/com/example/simpleoauth/SimpleOauthApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +33,8 @@ public class SimpleOauthApplication extends WebSecurityConfigurerAdapter{
 					.antMatchers("/", "/login**", "/webjars/**")
 					.permitAll()
 				.anyRequest()
-					.authenticated();
+					.authenticated()
+				.and().logout().logoutSuccessUrl("/").permitAll()	//logout
+				.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 	}
 }
