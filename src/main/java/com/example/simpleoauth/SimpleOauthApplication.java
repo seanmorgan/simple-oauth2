1058e9cc33d7ec8959fe92c9c321ccf7a0001f74
seanmorgan <seanmorgan@gmail.com>
2018-01-17 12:02:09 -0500

    first commit

---
+++ src/main/java/com/example/simpleoauth/SimpleOauthApplication.java
@@ -0,0 +1,12 @@
+package com.example.simpleoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleOauthApplication.class, args);
+	}
+}
